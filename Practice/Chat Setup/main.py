2c6230e49d8dc84d23ad49c37df31295e7d57330
Akhil562! <akhilraghavakalal@gmail.com>
2025-01-22 18:39:57 -0600

    created a chat like system where input from user is taken as query and assistant responses are appeneded to the list and sent to the chat to create context

---
+++ Practice/Chat Setup/main.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# creating the client
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+# user message
+user_queries = []
+
+while True:
+    print("Enter the query: ")
+    user_message = input()
+    if user_message.lower() == "quit":
+        break
+    else:
+        user_queries.append({"role": "user", "content": user_message})
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=user_queries,
+            max_tokens=100,  # setting up this max tokens to handle the cost utilization
+        )
+
+        llm_response = response.choices[0].message.content
+        print("\nResponse: \n", llm_response, "\n")
+        user_queries.append({"role": "assistant", "content": llm_response})
+
+print("\nChat details: \n",user_queries)
